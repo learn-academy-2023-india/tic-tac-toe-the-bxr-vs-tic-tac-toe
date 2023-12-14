547606745abb74a4a38a914e82ac49be2b43e846
RoderickVongsa <roderick.vongsa@gmail.com>
2023-12-14 11:36:28 -0800

    story 1 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
+// import XPlayer from "./components/XPlayer"
+// import OPlayer from "./components/OPlayer"
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [currentPlayer, setCurrentPlayer] = useState('❌')
+
+
+
+const winner = () => {
+  if (Square[1] === Square[2]){
+    return "win"
+  }
+  else {
+    return "not win"
+  }
+//   let winningStatus = () => {
+
+//   }
+}
+
+// const [winner, setWinner] = useState([
+//   { player: ❌, winning: false },
+//   { player: ⭕️, winning: false }
+// ])
+
+
+const handleClick = (id) => {
+  if (board[id] === null) {
+    const newBoard = [...board]
+    newBoard[id] = currentPlayer
+    setBoard(newBoard)
+    setCurrentPlayer(currentPlayer === '❌' ? '⭕️' : '❌')
+  }
+
+}
+
+
+
+const restartGame = () => {
+  setBoard(Array(9).fill(null))
+  setCurrentPlayer('❌')
+}
+
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <Square board = {board}
+      handleClick ={handleClick}/>
+      {/* <XPlayer board = {board}/>
+      <OPlayer board = {board}/> */}
+      <h3> Player {winner} won!!</h3>
+      <button onClick = {restartGame}>Restart Game</button>
+     
     </>
   )
 }
